3849eb2a22df7d7355db77c1964a74b9588bdaa9
Jay <walkccray@gmail.com>
2018-08-21 14:10:26 +0800

    update

---
+++ Assets/DataFiles/Scripts/AutoWalk.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AutoWalk : MonoBehaviour {
+	public int speed = 10;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = transform.position + Camera.main.transform.forward * speed * Time.deltaTime;
+	}
+
+	public void OnTriggerEnter(Collider collide) {
+		if (collide.gameObject.CompareTag ("Trophy")) {
+			SceneManager.LoadScene ("MainScene");
+		}
+	}
+}
